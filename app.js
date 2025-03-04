79652b8bac1bb8ebef43b339e192f11eed67b0f9
HaiileGebriel <yesexamle972@gmail.com>
2025-03-04 13:16:23 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,67 @@
+import express, { json } from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+
+
+dotenv.config();
+const app = express()
+const port = process.env.PORT;
+const MONGOURL = process.env.MONGO_URL;
+app.use(express.json())
+
+const JobSchema = new mongoose.Schema({
+    title:String,
+    type:String,
+    salary:Number,
+    description:String,
+    company:String,
+    logo:String,
+    isBookMarked:Boolean,
+    location:String,
+    experienceLevel:String,
+    currency:String,
+})
+
+const connectDB = async()=>{
+    try{
+        await mongoose.connect(MONGOURL);
+        console.log("✅ database is connected successfully")
+    }catch(err){
+        console.log("❌ database is failed to connect")
+        process.exit(1)
+    }
+}
+
+const jobs = mongoose.model("jobs", JobSchema)
+
+connectDB();
+
+app.get('/',(req,res)=>{
+    res.send("Working...")
+})
+
+app.get('/jobs',async(req,res)=>{
+    const job = await jobs.find()
+    res.json(job)
+})
+
+app.get('/jobs/:id',async(req,res)=>{
+    const item = await jobs.findById(req.params.id)
+    res.json(item)
+})
+
+app.delete('/jobs/:id',async(req,res)=>{
+    const {id} = req.params.id
+    const item = await jobs.findOneAndDelete(id)
+    res.json(item)
+})
+
+app.post('/jobs',async(req,res)=>{
+    const newJob = await jobs.create(req.body)
+    res.json(newJob)
+    
+})
+
+
+
+app.listen(3000,()=>console.log("listening on localhost:3000"))
